43b76f17ca31e328b3a1d09bfeeaeea1a971a9ba
Christopher <christophergm1999@gmail.com>
2022-05-08 21:19:38 +0200

    calendario modificado y hecho para el entrenador, casi hecho para el jugador

--- calendario.php
+++ calendario.php
@@ -6,86 +6,52 @@ include("db_connect.php");
 
 <!DOCTYPE html>
 <html>
+  <head>
+    <title>Calendar Demo</title>
+    <link rel="stylesheet" href="css/calendar.css">
+    <script src="calendar.js"></script>
+  </head>
+  <body>
+    <!-- (A) PERIOD SELECTOR -->
+    <div id="calPeriod"><?php
+      // (A1) MONTH SELECTOR
+      // NOTE: DEFAULT TO CURRENT SERVER MONTH YEAR
+      $months = [
+        1 => "January", 2 => "Febuary", 3 => "March", 4 => "April",
+        5 => "May", 6 => "June", 7 => "July", 8 => "August",
+        9 => "September", 10 => "October", 11 => "November", 12 => "December"
+      ];
+      $monthNow = date("m");
+      echo "<select id='calmonth'>";
+      foreach ($months as $m=>$mth) {
+        printf("<option value='%s'%s>%s</option>",
+          $m, $m==$monthNow?" selected":"", $mth
+        );
+      }
+      echo "</select>";
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+      // (A2) YEAR SELECTOR
+      echo "<input type='number' id='calyear' value='".date("Y")."'/>";
+    ?></div>
 
-	<!--hoja de estilos -->
-	<link type="text/css" href="css/estilo.css" rel="stylesheet" />
-	<link type="text/css" href="css/sydebar.css" rel="stylesheet" />
+    <!-- (B) CALENDAR WRAPPER -->
+    <div id="calwrap"></div>
 
-	<!-- jQuery -->
-	<link rel="stylesheet" href="css/calendar.css">
-
-	<!-- Boxicons CDN Link -->
-	<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-</head>
-
-<body class="">
-
-	<?php
-	//Conectamos con la BD
-	$link = conectar();
-	?>
-
-	<div class="sidebar">
-		<div class="logo-details">
-			<div class="logo_name">JustVoley</div>
-			<i class='bx bx-menu' id="btn"></i>
-		</div>
-		<ul class="nav-list">
-			<li>
-				<a href="inicioJugador.php">
-					<i class='bx bx-user'></i>
-					<span class="links_name">Inicio</span>
-				</a>
-				<span class="tooltip">Inicio</span>
-			</li>
-			<li>
-				<a href="salir.php">
-					<i class='bx bx-log-out' id="log_out"></i>
-					<span class="links_name">Cerrar sesión</span>
-				</a>
-				<span class="tooltip">Cerrar sesión</span>
-		</ul>
-	</div>
-	<div class="container" style="min-height:500px;">
-		<div class=''>
-		</div>
-		<div class="container">
-			<h2>Calendario</h2>
-			<div class="page-header">
-				<div class="pull-right form-inline">
-					<div class="btn-group">
-						<button class="btn btn-primary" data-calendar-nav="prev">
-							<< Anterior</button>
-								<button class="btn btn-default" data-calendar-nav="today">Hoy</button>
-								<button class="btn btn-primary" data-calendar-nav="next">Siguiente >></button>
-					</div>
-					<div class="btn-group">
-						<button class="btn btn-warning" data-calendar-view="year">Año</button>
-						<button class="btn btn-warning active" data-calendar-view="month">Mes</button>
-						<button class="btn btn-warning" data-calendar-view="week">Semana</button>
-						<button class="btn btn-warning" data-calendar-view="day">Día</button>
-					</div>
-				</div>
-				<h3></h3>
-			</div>
-			<div class="row">
-				<div class="col-md-9">
-					<div id="showEventCalendar"></div>
-				</div>
-				<div class="col-md-3">
-					<h4>Lista de eventos</h4>
-					<ul id="eventlist" class="nav nav-list"></ul>
-				</div>
-			</div>
-		</div>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
-		<script type="text/javascript" src="js/calendar.js"></script>
-		<script type="text/javascript" src="js/events.js"></script>
-		<script src="js/sydebar.js"></script>
+    <!-- (C) EVENT FORM -->
+    <div id="calblock"><form id="calform">
+      <input type="hidden" name="req" value="save"/>
+      <input type="hidden" id="evtid" name="eid"/>
+      <label for="start">Date Start</label>
+      <input type="datetime-local" id="evtstart" name="start" required/>
+      <label for="end">Date End</label>
+      <input type="datetime-local" id="evtend" name="end" required/>
+      <label for="txt">Event</label>
+      <textarea id="evttxt" name="txt" required></textarea>
+      <label for="color">Color</label>
+      <input type="color" id="evtcolor" name="color" value="#e4edff" required/>
+      <input type="submit" id="calformsave" value="Save"/>
+      <input type="button" id="calformdel" value="Delete"/>
+      <input type="button" id="calformcx" value="Cancel"/>
+    </form></div>
+  </body>
+</html>
