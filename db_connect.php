2bce92ce64bfabedee03fc0d62ca04b81d76e9b5
Christopher <christophergm1999@gmail.com>
2022-05-10 23:18:15 +0200

    cambio de credenciales

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 //Función que se conecta a la BD y devuelve el link
 function conectar(){
-    if(!($link=mysqli_connect("localhost","root","","club"))){
+    if(!($link=mysqli_connect("185.42.104.113","myjustvole","admin123+","justvoley"))){
         $_SESSION["error"]="Imposible conectar con servidor MySQL o la base de datos";
         exit();
         }
