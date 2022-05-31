a1f24c729f923b8be48a46c21b1810abc75e0e25
Alejandro Aguilar Otero <alexakarex@gmail.com>
2022-05-31 19:31:43 +0200

    cambio conexion bd

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,10 @@
 <?php
 //Función que se conecta a la BD y devuelve el link
 define("DB_HOST", "localhost");
-define("DB_NAME", "club");
+define("DB_NAME", "justvoley");
 define("DB_CHARSET", "utf8");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
+define("DB_USER", "myjustvole");
+define("DB_PASSWORD", "admin123+");
 function conectar(){
     if(!($link=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME))){
         $_SESSION["error"]="Imposible conectar con servidor MySQL o la base de datos";
